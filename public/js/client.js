0944b16ed9179d03bb3858e877955989b1e821a5
YOURsammich <oel333@yahoo.com>
2014-09-28 17:51:09 -0500

    Update client.js

--- public/js/client.js
+++ public/js/client.js
@@ -966,7 +966,7 @@ parser = {
         // replace underscores, et cetera
         str = this.multiple(str, /\^([^\^]+?)\^/i, '<big>$1</big>');
         str = str.replace(/\*([^\s].+?[^\s])\*/g, '<strong>$1</strong>');
-        str = str.replace(/\&amp;([^\s].+?[^\s])\&amp;/g, '<marquee width="98%" behavior="alternate" direction="right">$1</marquee>');
+        str = str.replace(/\&amp;([^\s].+?[^\s])\&amp;/g, '<div id=marquee>$1</div>');
         str = this.multiple(str, /&#126;([^&#126;]+?)&#126;/i, '<small>$1</small>');
         str = str.replace(/`([^\s].+?[^\s])`/g, '<code>$1</code>');
         // try to replace all >>>/x/??? for links to boards.4chan.org/x/res/???
