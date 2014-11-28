a5f036fe16feff4029f63befc77c2e84aa335f98
YOURsammich <oel333@yahoo.com>
2014-11-28 03:42:03 -0600

    menu update

--- public/js/client.js
+++ public/js/client.js
@@ -461,7 +461,7 @@ $(function() {
     ONLINE.on('reset', function() {
         $('#online').html('');
     });
-    $('#online-users').draggable();
+    $('#online-users').draggable({ containment: '#messages' });
 });
 
 // ------------------------------------------------------------------
