db9af2a03a159e39bb9321c16d04c86a4589e542
Sammich <oel333@yahoo.com>
2014-11-29 00:20:46 -0600

    Update client.js

--- public/js/client.js
+++ public/js/client.js
@@ -461,7 +461,7 @@ $(function() {
     ONLINE.on('reset', function() {
         $('#online').html('');
     });
-    $('#online-users').draggable({ containment: 'window' }).resizable();;
+    $('#online-users').draggable({ containment: '#messages' }).resizable();;
 });
 
 // ------------------------------------------------------------------
