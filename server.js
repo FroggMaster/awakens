93692daa648d8430f606f09604d91938be2ad8af
Sammich <oel333@yahoo.com>
2015-02-09 21:40:46 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1174,6 +1174,9 @@ function initApp(app, server, https) {
             if(httpsDomain){
             	var port = httpsPort == 443 ? '' : ':' + httpsPort;
             	res.redirect('https://' + domain + port + req.url);
+            } else {
+            	var port = httpsPort == 80 ? '' : ':' + httpPort;
+            	res.redirect('http://' + domain + port + req.url);
             }
         } else {
             try {
