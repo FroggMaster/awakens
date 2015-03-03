700d2f73d26391422f254046344393b5b0c78aab
InfraRaven <InfraRaven@gmail.com>
2015-03-03 23:59:15 +1000

    /set bg
    changing some minor inline css of the background

--- public/js/client.js
+++ public/js/client.js
@@ -467,7 +467,7 @@ $(function() {
         if(bg == 'on'){
             $('#messages').css('background', CLIENT.get('old'));
         } else {
-            $('#messages').css('background', 'url(http://i.imgur.com/b9xE8sb.png?1) center / auto 100% no-repeat rgb(17, 17, 17)');
+            $('#messages').css('background', 'url(./public/css/img/bg.png) center / auto 50% no-repeat rgb(17, 17, 17)');
         }
     });
 });
