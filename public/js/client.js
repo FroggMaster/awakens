ab193fadb6bc3f9410cc06c33e106eb8851e5573
RandomsDude <ilhanbok@gmail.com>
2015-04-05 19:39:32 -0500

    Fix javascript links (in my own odd way)
    It uses onClick BUT it's SAFE, because it has href="javascript:void(0)". According to Stackoverflow or something...

--- public/js/client.js
+++ public/js/client.js
@@ -1334,13 +1334,13 @@ parser = {
         // >
         str = str.replace(/^(&gt;)$/i, '&#35;789922 $1');
         //JavaScript links
-        /*str = str.replace(/(\/\?)([^\|]+)\|([^\|]+)\|?/gi, function(_, __, a, b){
-            if(/[^:]*javascript/im.test(a)) {
-                return '<div>' + b + '<a target="_blank" href="'+a+'">' + '[JavaScript]' + '</a></div>'
+        str = str.replace(/(\/\?)([^\|]+)\|([^\|]+)\|?/gi, function(_, __, a, b){
+            if(/[^:]*javascript *:/im.test(a)) {
+                    return '<div>' + b + '<a href="javascript:void(0)" title = "Run at your own risk. Running unknown javascript may compromise webpage security." onclick = "'+a+'">' + '[JavaScript]' + '</a></div>';
             } else {
-                return '<div><a target="_blank" href="'+a+'">' + b + '</a></div>'
+                return '<div>'+b+'<a href="javascript:void(0)" title = "Run at your own risk. Running unknown scripts may compromise webpage security." onclick = "javascript: '+a+'">' + '[Javascript?]' + '</a></div>'
             }
-        });*/
+        });
         //embed
         str = str.replace(/\/embed(\S*)(.*)/g, '<a target="_blank" href="$1">$1</a> <a target="_blank" onclick="video(\'\', \'embed\', \'$1\')">[embed]</a>');
         //colors
