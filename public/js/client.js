c8044e478c1960f081d64710c79ae90c84589e3e
RandomsDude <ilhanbok@gmail.com>
2015-04-17 17:29:26 -0500

    revert client.js edit theme style update

--- public/js/client.js
+++ public/js/client.js
@@ -498,8 +498,8 @@ $(function() {
         style = CLIENT.get('chat_style').split(',');
         $('#input-bar').css('background-color', style[0]);
         if(navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
-            document.styleSheets[1].deleteRule(9);
-            document.styleSheets[1].insertRule(".scrollbar_default::-webkit-scrollbar-thumb { border-radius: 5px; background: " + style[1] + "",9);
+            document.styleSheets[1].deleteRule(8);
+            document.styleSheets[1].insertRule(".scrollbar_default::-webkit-scrollbar-thumb { border-radius: 5px; background: " + style[1] + "",8);
         }
     });
 });
