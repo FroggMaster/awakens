2c8a3649b629a34b130ff21d90fba50b9ca8453b
1337technomancer <1337technomancer@gmail.com>
2015-05-11 22:32:00 -0500

    fixed voice url
    removed /public from url

--- server.js
+++ server.js
@@ -714,7 +714,7 @@ function createChannel(io, channelName) {
                             if (t === undefined) {
                                 t = settings.speak['default'];
                             }
-                            request('http://this.spooks.me/public/audio/speak/' + params.voice + 'speak.php?text=' + encodeURIComponent(params.message), function (error, response, body) {
+                            request('http://this.spooks.me/audio/speak/' + params.voice + 'speak.php?text=' + encodeURIComponent(params.message), function (error, response, body) {
                                 if(voice == 'default') {
                                     body = null
                                 };
