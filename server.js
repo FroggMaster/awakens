9c2d92f1a3f4a042c8814465d3b469f1c07fcaa2
bitchnigga <mrguy@spam4.me>
2015-06-14 21:10:09 -0700

    I swear it will work this time

--- server.js
+++ server.js
@@ -1209,6 +1209,9 @@ function createChannel(io, channelName) {
                     var hat = Math.random() < 0.0001 ? 'Gold' : Math.random() < 0.001 ? 'Coin' : 'nohat';
                     var message = msg && msg.message;
                     if (typeof message == 'string') {
+						if (message.contains("watch?v=") || message.contains("youtu.be")) {
+						    getTitles(message);
+						}
                         dao.findUser(user.nick).done(function(dbuser) {
                             if (user.role != 'mute') {
                                 count++;
