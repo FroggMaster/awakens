603b4d8429b7a2a484fe74be535ce5980f114f8f
RandomsDude <ilhanbok@gmail.com>
2015-06-25 20:48:48 -0500

    Channels p2

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ var fs = require('fs');
 var httpsPort = settings.https && settings.https.port;
 var httpPort = settings.server.port;
 
+var channelList = [];
+
 String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
 
 /*
@@ -27,6 +29,7 @@ process.on('uncaughtException', function(err) {
 });
 
 function createChannel(io, channelName) {
+    channelList.push(channelName);
     console.log('Starting channel: ' + (channelName || '<fontpage>'));
 
     var elbot = require('./elbot').start();
@@ -49,6 +52,9 @@ function createChannel(io, channelName) {
     };
     
     room.on('connection', function(socket) {
+        
+        if (channelList.indexOf(channelName) == -1)
+            channelList.push(channelName);
         
         var user = {
             remote_addr : socket.request.connection.remoteAddress,
@@ -115,6 +121,8 @@ function createChannel(io, channelName) {
                         part : user.part,
                         kicked : user.kicked
                     });
+                    if (channel.online.length == 0 && channelList.indexOf(channelName) != -1)
+                        channelList.splice(channelList.indexOf(channelName), 1);
                 }
                 //log.info('Disconnected');
             } catch (err) {
@@ -1045,6 +1053,16 @@ function createChannel(io, channelName) {
                             frame_src : params.url
                         });
                     });
+                }
+            },
+            channels : {
+                role : 'super',
+                handler: function(params) {
+                    var msg = '';
+                    for (var i = 0; i < channelList.length; i++){
+                        msg += channelList[i] + '\n';
+                    }
+                    showMessage('Channels:\n' + msg);
                 }
             },
             ask : {
