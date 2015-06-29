2587e3e5db4023c12e36fc145b3169072db6762e
Michael Elbert <mrguy@spam4.me>
2015-06-28 17:48:55 -0700

    /whereis

--- server.js
+++ server.js
@@ -1141,7 +1141,27 @@ function createChannel(io, channelName) {
                         errorMessage('That hat doesn\'t exist.')
                     }
                 }
-            }
+            },
+	whereis: {
+	    params: ['nick'],
+	    handler: function (dao, dbuser, params) {
+	        return dao.findUser(params.nick).then(function (dbuser) {
+	            var stats = grab(params.nick);
+	            if (stats != -1 || dbuser) {
+	                if (dbuser) {
+	                    return dao.find_ip(dbuser.get('remote_addr')).then(function (nicks) {
+	                        whereIs(dbuser.get('remote_addr'))
+	                        return true;
+	                    });
+	                } else {
+	                    return false;
+	                }
+	            } else {
+	                return $.Deferred().resolve(false, msgs.get('user_doesnt_exist', params.nick));
+	            }
+	        });
+	    }
+	}
         };
 
         // -----------------------------------------------------------------------------
