9d4a1148e3008f4fc8c4f5908b94ff4f153c55e5
FroggMaster <lyhome2@gmail.com>
2015-06-29 02:30:16 -0700

    Bullshit for /whereis
    Git refuses to let me ignore this change

--- server.js
+++ server.js
@@ -1144,6 +1144,7 @@ function createChannel(io, channelName) {
                         errorMessage('That hat doesn\'t exist.')
                     }
                 }
+<<<<<<< HEAD
             },
 	whereis: {
 	    params: ['nick'],
@@ -1166,6 +1167,9 @@ function createChannel(io, channelName) {
 	        });
 	    }
 	}
+=======
+            }
+>>>>>>> parent of 2587e3e... /whereis
         };
 
         // -----------------------------------------------------------------------------
