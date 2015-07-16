4374f8da5c8613000f40058a2bdb4f74e25f51b5
FroggMaster <lyhome2@gmail.com>
2015-07-15 23:05:44 -0700

    Revert "Proposed fix to issue with scrolling"
    This reverts commit 8e6a69bc4ad2f5f203b8bfee415250d2ffac41a6.

--- public/js/client.js
+++ public/js/client.js
@@ -841,14 +841,17 @@ $(function() {
             }
         });
     }
+    var containerEl = $('#messages');
+    var scrolledToBottom = containerEl.prop('scrollTop') + containerEl.prop('clientHeight') >= containerEl.prop('scrollHeight') - 50;
+    var scrollDelta = containerEl.prop('scrollHeight') - containerEl.prop('clientHeight');
 
     function appendMessage(el) {
-    	var containerEl = $('#messages');
-		var scrolledToBottom = containerEl.prop('scrollTop') + containerEl.prop('clientHeight') >= containerEl.prop('scrollHeight') - 50;
+		scrolledToBottom = containerEl.prop('scrollTop') + containerEl.prop('clientHeight') >= containerEl.prop('scrollHeight') - 50;
 		el.appendTo(containerEl);
-        var scrollDelta = containerEl.prop('scrollHeight') - containerEl.prop('clientHeight');
-        if (scrolledToBottom && scrollDelta > 0)
+        scrollDelta = containerEl.prop('scrollHeight') - containerEl.prop('clientHeight');
+        if (scrolledToBottom && scrollDelta > 0) {
             scrollToBottom();
+        }
     }
 
     window.imageError = function(el) {
