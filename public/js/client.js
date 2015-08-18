61dfeca359e72b46e501b45143cfe71b279ef601
Landon Powell <landonpowell@linuxmail.org>
2015-08-18 04:24:22 -0400

    Update client.js

--- public/js/client.js
+++ public/js/client.js
@@ -121,7 +121,7 @@ $(function() {
         if(url.url == "stop" || CLIENT.get('mute') == 'on' || CLIENT.get('play') == 'off') {
             $("#youtube")[0].innerHTML = "";
         } else {
-            $("#youtube")[0].innerHTML = "<iframe width=\"420\" height=\"345\" src=\"https://www.youtube.com/embed/" + url.url +"?autoplay=1\" frameborder=\"0\" allowfullscreen></iframe>"
+            $("#youtube")[0].innerHTML = "<iframe width=\"420\" height=\"345\" src=\"https://www.youtube.com/embed/" + url.url +"?autoplay=1\" frameborder=\"0\" allowfullscreen></iframe>";
         }
     });
 
