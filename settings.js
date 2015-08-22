31ecc02c2d3965fe81f56427758936f7e2ba87a6
Dom <FroggMaster@users.noreply.github.com>
2015-08-22 13:04:44 -0700

    Awakens Prep
    spooks references converted to Awakens

--- settings.js
+++ settings.js
@@ -18,7 +18,7 @@ module.exports = {
     },
 
     https : {
-        domain : '2spooks4.me',
+        domain : 'awakens.me',
         key : './ssl/myserver.key',
         cert : './ssl/server.crt',
         port : 443
@@ -102,7 +102,7 @@ module.exports = {
     },
 
     registrationEmail : {
-        from : 'Chat Server <donotreply@2spooks4.me>',
+        from : 'Chat Server <donotreply@awakens.me>',
         subject : 'Registering Chat Nickname',
         text : 'You are registering the nickname {0}.\r\nTo verify your account, all you have to do is type out the following: /verify {1}'
     },
